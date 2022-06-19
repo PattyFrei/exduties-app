f7168a7427ed607c53a266e626b1e9783fb77da8
PattyFrei <patty.github@pm.me>
2022-06-19 21:40:32 +0200

    update app component

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -4,9 +4,7 @@ import { AppComponent } from './app.component';
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [
-        AppComponent
-      ],
+      declarations: [AppComponent],
     }).compileComponents();
   });
 
@@ -16,16 +14,18 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'exduties-app'`, () => {
+  it(`should have as title 'ExDuties App'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('exduties-app');
+    expect(app.title).toEqual('ExDuties App');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('exduties-app app is running!');
+    expect(compiled.querySelector('.page-container h1')?.textContent).toContain(
+      'ExDuties App'
+    );
   });
 });
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,9 +5,10 @@ import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { ExchangeRateComponent } from './exchange-rate/exchange-rate.component';
+import { DutiesComponent } from './duties/duties.component';
 
 @NgModule({
-  declarations: [AppComponent, ExchangeRateComponent],
+  declarations: [AppComponent, ExchangeRateComponent, DutiesComponent],
   imports: [BrowserModule, HttpClientModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent],

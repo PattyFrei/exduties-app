7bcbe1b743dca8344e268abfb096c3e4059f87e1
PattyFrei <patty.github@pm.me>
2022-07-03 22:49:41 +0200

    update general styles

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  public title = 'ExDuties App';
+  public title = "Exchange'n'Duties App";
 }
